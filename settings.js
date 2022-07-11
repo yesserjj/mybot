c46714cf4a03bf5c27cfcea5456a7f7f6956cd86
JancugPristel <108045641+JancugPristel@users.noreply.github.com>
2022-07-12 02:28:17 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -70,7 +70,7 @@ global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Drekk Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "iDreki🗿" //ur sticker watermark author
+global.author = "iDreki🗿👆" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
